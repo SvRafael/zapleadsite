dc6fc146bee7bed421fc44176c5f33df20ef7ebd
SvRafael <rafaelsvsouza@hotmail.com>
2025-04-09 21:05:02 -0300

    feat: adiciona funcionalidades e caroulsel

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <script setup>
+import Caroulsel from './components/Caroulsel.vue';
+import Functions from './components/Functions.vue';
 import Header from './components/Header.vue';
 import Home from './components/Home.vue';
+import Section1 from './components/Section1.vue';
 </script>
 
 <template>
   <Header></Header>
   <main class="pt-16">
     <Home></Home>
-    
-    <section class="bg-gradient-to-b from-green-900 via-green-700 via-green-600 via-green-600 via-green-400 to-green-200 text-white w-full">
-      <div class="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between md:h-[calc(100vh-64px)] h-auto space-x-2">
-
-
-      <div class="md:w-1/2 w-full">
-        <img
-          src="https://zapleadoficial.com.br/wp-content/uploads/2025/02/Design-sem-nome-19.png"
-          alt="Recrutamento"
-          class="w-full h-auto rounded-xl object-cover"
-        />
-      </div>
-
-      <!-- Texto à direita com fundo branco -->
-      <div class="md:w-1/2 w-full bg-white p-8 rounded-xl shadow-md">
-        <h2 class="text-3xl font-bold text-gray-800 mb-4">Leve seu atendimento para o próximo nivel!</h2>
-        <p class="text-gray-600 text-lg mb-4">
-          Responda múltiplas conversas com facilidade, automatize o envio de mensagens e áudios como se fossem gravados na hora, deixando um atendimento leve e humanizado.
-        </p>
-        <label>ORGANIZAÇÃO = MAIS VENDAS</label>
-      </div>
-
-    </div>
-  </section>
+    <Section1></Section1>
+    <Functions></Functions>
+    <Caroulsel></Caroulsel>
   </main>
  
 </template>
---
+++ src/components/Caroulsel.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between h-auto space-x-2 py-12">
+    <div class="relative w-full h-full mx-auto overflow-hidden rounded-xl shadow-lg">
+      
+      <!-- Slides -->
+      <div
+        class="flex transition-transform duration-700 ease-in-out"
+        :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+      >
+        <div
+          v-for="(slide, index) in slides"
+          :key="index"
+          class="w-full flex-shrink-0"
+        >
+          <img :src="slide" class="w-full h-max object-cover" />
+        </div>
+      </div>
+  
+      <!-- Botões -->
+      <button
+        @click="prevSlide"
+        class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white/80 hover:bg-white text-gray-800 px-3 py-1 rounded-full shadow"
+      >
+        ‹
+      </button>
+  
+      <button
+        @click="nextSlide"
+        class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/80 hover:bg-white text-gray-800 px-3 py-1 rounded-full shadow"
+      >
+        ›
+      </button>
+  
+      <!-- Indicadores -->
+      <div class="absolute bottom-4 w-full flex justify-center gap-2">
+        <span
+          v-for="(slide, index) in slides"
+          :key="index"
+          class="w-3 h-3 rounded-full"
+          :class="currentIndex === index ? 'bg-white' : 'bg-white/50'"
+        />
+      </div>
+    </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const slides = ref([
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/4-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/Selecione-a-etiqueta-desejada-para-que-os-contatos-dessa-etiqueta-possam-ser-listados-para-o-envio-das-mensagem-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/5-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/2-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/Selecione-a-etiqueta-desejada-para-que-os-contatos-dessa-etiqueta-possam-ser-listados-para-o-envio-das-mensagem-1-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/1-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/Selecione-a-etiqueta-desejada-para-que-os-contatos-dessa-etiqueta-possam-ser-listados-para-o-envio-das-mensagem-2-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/Selecione-a-etiqueta-desejada-para-que-os-contatos-dessa-etiqueta-possam-ser-listados-para-o-envio-das-mensagem-4-1024x640.png",
+  "https://zapleadoficial.com.br/wp-content/uploads/2025/02/Selecione-a-etiqueta-desejada-para-que-os-contatos-dessa-etiqueta-possam-ser-listados-para-o-envio-das-mensagem-3-1024x640.png"
+])
+  
+  const currentIndex = ref(0)
+  
+  function nextSlide() {
+    currentIndex.value = (currentIndex.value + 1) % slides.value.length
+  }
+  
+  function prevSlide() {
+    currentIndex.value =
+      (currentIndex.value - 1 + slides.value.length) % slides.value.length
+  }
+  </script>
+  
